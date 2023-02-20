4d66dcbd4620bf72df1783e1ad5df1c61fff1a4e
desertgopher <quasar360@mail.ru>
2023-02-20 16:31:22 +0300

    did not finish

--- SHA-256/sha-256_lib.py
+++ SHA-256/sha-256_lib.py
@@ -2,5 +2,5 @@ import hashlib
 
 message = input(str("Phrase to hash: "))
 
-hashed_message = hashlib.sha256(message.encode('utf-8')).hexdigest()
-print(hashed_message)
+print(hashlib.sha256(message.encode('utf-8')).hexdigest())
+
